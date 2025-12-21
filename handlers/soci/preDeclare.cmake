83b2b6c6673fdb21f28c8ad01fe56d0a74a6cbb4
gh <gethro2k@gmail.com>
2025-12-21 20:17:36 +1000

    m

--- handlers/soci/preDeclare.cmake
+++ handlers/soci/preDeclare.cmake
@@ -1,10 +1,4 @@
 function(soci_preDownload pkgname url tag srcDir)
-    message(NOTICE "set(CMAKE_POLICY_DEFAULT_CMP0077 ""NEW"")")
-    set(CMAKE_POLICY_DEFAULT_CMP0077 "NEW")
-    message(NOTICE "set(SOCI_SQLITE3_AUTO ON)")
-    set(SOCI_SQLITE3_AUTO ON)
-    message(NOTICE "set(SOCI_SQLITE3_BUILTIN ""prefer"" CACHE STRING ""Prefer, or forbid, using the built-in SQLite3 library"" FORCE)")
-    set(SOCI_SQLITE3_BUILTIN "PREFER" CACHE STRING "Prefer, or forbid, using the built-in SQLite3 library" FORCE)
 endfunction()
 
 soci_preDownload(${this_pkgname} ${this_url} ${this_tag} "${EXTERNALS_DIR}/${this_pkgname}")
