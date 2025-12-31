b251ca411af76c33b7656a37d330ad48bd713a04
gh <gethro2k@gmail.com>
2025-12-31 21:52:40 +1000

    thing

--- handlers/OpenSSL/process.cmake
+++ handlers/OpenSSL/process.cmake
@@ -3,16 +3,19 @@ function(OpenSSL_process incs libs defs)
         find_library(OpenSSL REQUIRED
                 NAMES ssl
                 PATHS "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MT")
+        find_library(OpenSSL_Crypto REQUIRED
+                NAMES crypto
+                PATHS "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MT")
+        list(APPEND _LibrariesList OpenSSL OpenSSL_Crypto)
+        set (_LibrariesList ${_LibrariesList} PARENT_SCOPE)
     elseif (APPLE)
         set(ENV{OPENSSL_DIR} "${OPENSSL_PATH}")
         find_package(OpenSSL CONFIG REQUIRED)
+        list(APPEND _LibrariesList OpenSSL::SSL OpenSSL::Crypto)
+        set (_LibrariesList ${_LibrariesList} PARENT_SCOPE)
     else ()
         message(FATAL_ERROR "OpenSSL for Linux should not use \"process\"")
     endif ()
-
-    list(APPEND _LibrariesList OpenSSL::SSL OpenSSL::Crypto)
-    set (_LibrariesList ${_LibrariesList} PARENT_SCOPE)
-    # @formatter:on
 endfunction()
 
 if (SSL IN_LIST APP_FEATURES)
