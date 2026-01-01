2e90cb904ed4c27cfc6043a9ced9a362b46e5f9c
gh <gethro2k@gmail.com>
2026-01-02 03:40:24 +1000

    thing

--- handlers/soci/fix.cmake
+++ handlers/soci/fix.cmake
@@ -20,6 +20,30 @@ function(soci_fix target tag sourceDir)
     ReplaceInFile("${sourceDir}/src/core/soci-simple.cpp" " ssize" " soci::ssize")
     ReplaceInFile("${sourceDir}/src/core/statement.cpp" " ssize" " soci::ssize")
 
+    # Patch blob.h to move destructor to the .cpp file, fixing the incomplete type error with dllexport
+    set(BLOB_H "${EXTERNALS_DIR}/soci/include/soci/blob.h")
+    set(BLOB_CPP "${EXTERNALS_DIR}/soci/src/core/blob.cpp")
+
+    # 1. Remove the inline default destructor and constructor from the header
+    ReplaceInFile(${BLOB_H} "blob() = default;" "blob();")
+    ReplaceInFile(${BLOB_H} "~blob() = default;" "~blob();")
+    ReplaceInFile(${BLOB_H} "blob(blob &&other) = default;" "blob(blob &&other);")
+    ReplaceInFile(${BLOB_H} "blob &operator=(blob &&other) = default;" "blob &operator=(blob &&other);")
+
+    # 2. Add the implementations to the .cpp file where blob_backend is (usually) included or defined
+    ReplaceInFile(${BLOB_CPP} "blob::~blob() = default;\n" "")
+    ReplaceInFile(${BLOB_CPP} "soci::blob::blob() {}\n" "")
+    ReplaceInFile(${BLOB_CPP} "soci::blob::~blob() {}\n" "")
+    ReplaceInFile(${BLOB_CPP} "soci::blob::blob(blob &&other) noexcept {};\n" "")
+    ReplaceInFile(${BLOB_CPP} "soci::blob &soci::blob::operator=(blob &&other) noexcept = default;\n" "")
+
+    file(APPEND ${BLOB_CPP}
+            "soci::blob::blob() {}\n"
+            "soci::blob::~blob() {}\n"
+            "soci::blob::blob(blob &&other) noexcept {};\n"
+            "soci::blob &soci::blob::operator=(blob &&other) noexcept = default;\n"
+    )
+
 endfunction()
 
 soci_fix("${this_pkgname}" "${this_tag}" "${this_src}")
