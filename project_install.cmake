9bc97cd4b6412064d893680a053c846b4c7a9458
gh <gethro2k@gmail.com>
2026-01-05 17:56:25 +1000

    thing

--- project_install.cmake
+++ project_install.cmake
@@ -42,9 +42,9 @@ include(${CMAKE_SOURCE_DIR}/cmake/generate_app_config.cmake)
 include(ExternalProject)
 if (APP_INCLUDES_RESOURCES OR APP_SUPPLIES_RESOURCES)
     set(RES_DIR "${CMAKE_CURRENT_SOURCE_DIR}/resources")
-    if (APP_SUPPLIES_RESOURCES)
+    if (APP_CONSUMES_RESOURCES)
         ExternalProject_Add(${APP_NAME}ResourceRepo
-                GIT_REPOSITORY "${APP_SUPPLIES_RESOURCES}"
+                GIT_REPOSITORY "${APP_CONSUMES_RESOURCES}"
                 GIT_TAG master
                 GIT_SHALLOW TRUE
                 UPDATE_DISCONNECTED TRUE
