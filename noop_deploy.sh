7e9776aacc4479de7889efea7a31771ddfc9a553
geoffrey <gethro2k@gmail.com>
2026-01-06 15:06:55 +1000

    thing

--- noop_deploy.sh
+++ noop_deploy.sh
@@ -1,20 +1,20 @@
 : <<'BATCH'
 @echo off
 rem This section is only seen by Windows CMD
-if exist "noop.cmd" (
-    call noop.cmd
+if exist "cmake\noop.cmd" (
+    call cmake\noop.cmd
 ) else (
-    echo [ERROR] noop.cmd not found.
+    echo [ERROR] cmake\noop.cmd not found.
     exit /b 1
 )
 exit /b %ERRORLEVEL%
 BATCH
 
 # This section is only seen by Bash (Linux/macOS)
-if [ -f "./noop.sh" ]; then
-    chmod +x ./noop.sh
-    ./noop.sh
+if [ -f "./cmake/noop.sh" ]; then
+    chmod +x ./cmake/noop.sh
+    ./cmake/noop.sh
 else
-    echo "[ERROR] noop.sh not found."
+    echo "[ERROR] ./cmake/noop.sh not found."
     exit 1
 fi
