a1a54cd1ceae2a9dd47026a3a80fa8dd3aae41b4
gh <gethro2k@gmail.com>
2026-01-14 00:49:46 +1000

    Win11

--- handlers/wxWidgets/postAddTarget.cmake
+++ handlers/wxWidgets/postAddTarget.cmake
@@ -15,3 +15,23 @@ if (link_libs)
         set_target_properties(${target} PROPERTIES INTERFACE_LINK_LIBRARIES "${new_link_libs}")
     endif()
 endif()
+
+# This handler is called by addTarget for every target created/found for the package
+if (WIN32)
+    # List of internal wxWidgets targets that should not be exported
+    set(_wx_internal_targets
+            wxregex wxzlib wxexpat wxjpeg wxpng wxtiff
+            wxscintilla wxlexilla
+    )
+
+    # If the current target is one of these, remove it from the dependencies list
+    # so it doesn't trigger "not in export set" errors during installation.
+    foreach(_internal IN LISTS _wx_internal_targets)
+        if ("${target}" MATCHES "${_internal}")
+            message(STATUS "wxWidgets: Filtering internal target '${target}' from export set")
+            list(REMOVE_ITEM _DependenciesList ${target})
+            set(_DependenciesList ${_DependenciesList} PARENT_SCOPE)
+            break()
+        endif()
+    endforeach()
+endif()
--- handlers/wxWidgets/postMakeAvailable.cmake
+++ handlers/wxWidgets/postMakeAvailable.cmake
@@ -1,3 +1,8 @@
 include(${CMAKE_CURRENT_LIST_DIR}/helpers.cmake)
 wxWidgets_export_variables(${this_pkgname})
+
+# Synchronize with the variables expected by fetchContents.cmake
+set(_wxIncludePaths ${_wxIncludePaths} PARENT_SCOPE)
+set(_wxLibraries    ${_wxLibraries}    PARENT_SCOPE)
+
 set(HANDLED ON PARENT_SCOPE)
