39914f025532f6917ff0c18c81273fac68f822b3
gh <gethro2k@gmail.com>
2026-01-14 01:57:18 +1000

    Wed Jan 14 01:57:18 EAST 2026

--- handlers/wxWidgets/postMakeAvailable.cmake
+++ handlers/wxWidgets/postMakeAvailable.cmake
@@ -2,7 +2,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/helpers.cmake)
 wxWidgets_export_variables(${this_pkgname})
 
 # Synchronize with the variables expected by fetchContents.cmake
-set(_wxIncludePaths ${_wxIncludePaths} PARENT_SCOPE)
-set(_wxLibraries    ${_wxLibraries}    PARENT_SCOPE)
+#set(_wxIncludePaths ${_wxIncludePaths} )
+#set(_wxLibraries    ${_wxLibraries}    )
 
-set(HANDLED ON PARENT_SCOPE)
+set(HANDLED ON)
