0b191e271160d000702e5982922a3b55e38d097b
gh <gethro2k@gmail.com>
2026-01-17 01:23:32 +1000

    thing

--- handlers/soci/preDownload.cmake
+++ handlers/soci/preDownload.cmake
@@ -25,12 +25,22 @@ function(soci_preDownload pkgname url tag srcDir)
     set(SOCI_POSTGRESQL     "OFF" CACHE STRING "Disable SOCI PostgreSQL backend" FORCE)
 
     # 1. Fetch fmt first with install enabled
+    message(STATUS [=[
+
+    FetchContent_Declare(
+            fmt
+            GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+            GIT_TAG 12.1.0
+    )]=])
     FetchContent_Declare(
             fmt
             GIT_REPOSITORY https://github.com/fmtlib/fmt.git
             GIT_TAG 12.1.0
     )
+
     set(FMT_INSTALL ON CACHE BOOL "" FORCE)
+
+    message(STATUS "FetchContent_MakeAvailable(fmt)")
     FetchContent_MakeAvailable(fmt)
 
     # 2. Point SOCI to our fmt installation
