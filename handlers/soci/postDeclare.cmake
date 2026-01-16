920598bda49049b32bcdcbd5692a5108b667a99b
gh <gethro2k@gmail.com>
2026-01-17 00:06:43 +1000

    thing

--- handlers/soci/postDeclare.cmake
+++ handlers/soci/postDeclare.cmake
@@ -1,8 +1,12 @@
 function(soci_postDeclare pkgname)
 
-    if(SKIP_INSTALL_RULES)
-        set(CMAKE_SKIP_INSTALL_RULES OFF CACHE BOOL "" FORCE)
-        unset(SKIP_INSTALL_RULES CACHE)
-    endif ()
+    FetchContent_GetProperties(soci)
+    if(NOT soci_POPULATED)
+        FetchContent_Populate(soci)
+
+        # Disable install in the subdirectory
+        set(SOCI_INSTALL OFF)
+        add_subdirectory(${soci_SOURCE_DIR} ${soci_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
 
 endfunction()
---
+++ handlers/soci/preMakeAvailable.cmake
@@ -0,0 +1,5 @@
+function(soci_postMakeAvailable pkgname)
+
+    set(HANDLED ON PARENT_SCOPE)
+
+endfunction()
