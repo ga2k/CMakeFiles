8a914a91cd4b9a835dbf5816b6e9dda245f0583b
gh <gethro2k@gmail.com>
2026-01-17 20:27:21 +1000

    thing

--- handlers/soci/preDownload.cmake
+++ handlers/soci/preDownload.cmake
@@ -30,12 +30,12 @@ function(soci_preDownload pkgname url tag srcDir)
     FetchContent_Declare(
             fmt
             GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-            GIT_TAG 12.1.0
+            GIT_TAG 10.2.1
     )]=])
     FetchContent_Declare(
         fmt
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-        GIT_TAG 12.1.0
+        GIT_TAG 10.2.1
     )
 
     set(FMT_INSTALL ON CACHE BOOL "" FORCE)
