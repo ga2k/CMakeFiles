36414cfa1765adcb2b59382191fd20c99a3b34b0
gh <gethro2k@gmail.com>
2026-01-17 12:35:48 +1000

    Sat Jan 17 12:35:48 EAST 2026

--- handlers/soci/preDownload.cmake
+++ handlers/soci/preDownload.cmake
@@ -50,6 +50,7 @@ function(soci_preDownload pkgname url tag srcDir)
     set(SOCI_INSTALL OFF CACHE BOOL "Disable SOCI internal install" FORCE)
     set(SOCI_SQLITE3_BUILTIN ON CACHE BOOL "Prefer using built-in SQLite3" FORCE)
     set(SOCI_EXTERNAL_FMT ON CACHE BOOL "Use external fmt library" FORCE)
+    set(SOCI_FMT_BUILTIN OFF CACHE BOOL "Use external fmt library" FORCE)
 
     # @formatter:on
 
