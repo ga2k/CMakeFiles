26e4174e2908ac58d5541eb250817a5876e592b1
gh <gethro2k@gmail.com>
2026-01-23 15:26:12 +1000

    thing

--- handlers/soci/fix.cmake
+++ handlers/soci/fix.cmake
@@ -11,7 +11,7 @@ function(soci_fix target tag sourceDir)
             "soci/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/_deps/fmt-src/include/fmt/"
             "soci/include|${sourceDir}"
             "soci/CMakeLists.txt|${sourceDir}"
-            "soci/cmake/soci_define_backend_target.cmake|${sourceDir}/cmake"
+            "soci/cmake/soci_define_backend_target.cmake|${sourceDir}/cmake/"
             "soci/src/core/CMakeLists.txt|${sourceDir}/src/core"
             "soci/src|${sourceDir}"
     )
