15d0628fbae0425744d416a4663c91ccc13fbcbd
gh <gethro2k@gmail.com>
2026-01-26 01:40:46 +1000

    Auto-sync: 2026-01-26 01:40:45

--- handlers/soci/postAddTarget.cmake
+++ handlers/soci/postAddTarget.cmake
@@ -1,19 +1,23 @@
 set(soci_components_handled CACHE STRING "" FORCE)
-set(soci_components_needed HoffSoft::soci_core HoffSoft::soci_sqlite3 CACHE STRING "" FORCE)
+set(soci_components_needed soci_core soci_sqlite3 CACHE STRING "" FORCE)
+#set(soci_components_needed HoffSoft::soci_core HoffSoft::soci_sqlite3 CACHE STRING "" FORCE)
 
 function(soci_postAddTarget target)
     set(components_handled $CACHE{soci_components_handled})
     set(components_needed  $CACHE{soci_components_needed})
 
+    if("${target}" IN_LIST components_handled)
+        return()
+    endif ()
+
     list(APPEND components_handled ${target})
     list(REMOVE_ITEM components_needed ${target})
+    set(soci_components_handled ${components_handled} CACHE STRING "" FORCE)
+    set(soci_components_needed  ${components_needed}  CACHE STRING "" FORCE)
 
     if (NOT "${components_needed}" STREQUAL "")
         list(POP_FRONT components_needed next_soci_component)
-
-        set(soci_components_handled ${components_handled} CACHE STRING "" FORCE)
         set(soci_components_needed  ${components_needed}  CACHE STRING "" FORCE)
-
         addTargetProperties(${next_soci_component} soci ON)
     endif ()
 endfunction()
