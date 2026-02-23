81f5146488b04e07bf30341ac4436da53ebac94e
geoffrey <gethro2k@gmail.com>
2026-02-23 20:35:45 +1000

    Auto-sync: 2026-02-23 20:35:45

--- handlers/FindGfx/init.cmake
+++ handlers/FindGfx/init.cmake
@@ -1,19 +1,25 @@
 
 function(addGfxFeatures dry_run)
     # @formatter:off
-    addPackageData(PLUGIN FEATURE "APPEARANCE"  PKGNAME "Appearance"    METHOD "IGNORE" DRY_RUN ${dry_run})
-    addPackageData(PLUGIN FEATURE "LOGGER"      PKGNAME "Logger"        METHOD "IGNORE" DRY_RUN ${dry_run})
-    addPackageData(PLUGIN FEATURE "PRINT"       PKGNAME "Print"         METHOD "IGNORE" DRY_RUN ${dry_run})
-    addPackageData(SYSTEM FEATURE "WIDGETS"     PKGNAME "wxWidgets"     METHOD "FETCH_CONTENTS"
+    addPackageData(PLUGIN   FEATURE "APPEARANCE"  PKGNAME "Appearance"    METHOD "IGNORE" DRY_RUN ${dry_run})
+    addPackageData(PLUGIN   FEATURE "LOGGER"      PKGNAME "Logger"        METHOD "IGNORE" DRY_RUN ${dry_run})
+    addPackageData(PLUGIN   FEATURE "PRINT"       PKGNAME "Print"         METHOD "IGNORE" DRY_RUN ${dry_run})
+    addPackageData(OPTIONAL FEATURE "WIDGETS"     PKGNAME "wxWidgets"     METHOD "FETCH_CONTENTS"
             GIT_REPOSITORY "https://github.com/wxWidgets/wxWidgets.git" GIT_TAG "master"
             ARG REQUIRED DRY_RUN ${dry_run})
     addPackageData(LIBRARY FEATURE "GFX" PKGNAME "Gfx" METHOD "FIND_PACKAGE" NAMESPACE "HoffSoft"
             ARGS REQUIRED CONFIG PREREQ CORE DRY_RUN ${dry_run})
     # @formatter:on
 
-    registerPlugin("Appearance")
-    registerPlugin("Logger")
-    registerPlugin("Print")
+#    if(Appearance IN_LIST APP_FEATURES)
+        registerPlugin("Appearance")
+#    endif ()
+#    if(Logger IN_LIST APP_FEATURES)
+        registerPlugin("Logger")
+#    endif ()
+#    if(Print IN_LIST APP_FEATURES)
+        registerPlugin("Print")
+#    endif ()
 
 endfunction()
 
