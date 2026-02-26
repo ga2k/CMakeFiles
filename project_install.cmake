32ea4c9962e213dfac2a79be364e9757314b58f9
geoffrey <gethro2k@gmail.com>
2026-02-26 18:44:14 +1000

    Auto-sync: 2026-02-26 18:44:14

--- project_install.cmake
+++ project_install.cmake
@@ -306,17 +306,17 @@ add_custom_target(${APP_NAME}Config SOURCES "${cmake_root}/templates/Config.cmak
 add_dependencies(${APP_NAME} ${APP_NAME}Config)
 
 configure_package_config_file(
-        ${cmake_root}/templates/wxWidget.cmake.in
-        "${OUTPUT_DIR}/${APP_NAME}WX_Helper.cmake"
+        ${cmake_root}/templates/WX_Helper.cmake.in
+        "${OUTPUT_DIR}/WX_Helper.cmake"
         INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake
 )
-add_custom_target(${APP_NAME}WX_Helper SOURCES "${cmake_root}/templates/wxWidget.cmake.in")
+add_custom_target(${APP_NAME}WX_Helper SOURCES "${cmake_root}/templates/WX_Helper.cmake.in")
 add_dependencies(${APP_NAME} ${APP_NAME}WX_Helper)
 
 install(FILES
         "${OUTPUT_DIR}/${APP_NAME}Config.cmake"
         "${OUTPUT_DIR}/${APP_NAME}ConfigVersion.cmake"
-        "${OUTPUT_DIR}/${APP_NAME}WX_Helper.cmake"
+        "${OUTPUT_DIR}/WX_Helper.cmake"
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake
 )
 
