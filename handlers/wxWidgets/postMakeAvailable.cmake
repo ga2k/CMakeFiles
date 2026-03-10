648e289b22d8854e06d3b3fbe9568fcb2025fce6
geoffrey <gethro2k@gmail.com>
2026-03-10 21:17:04 +1000

    Auto-sync: 2026-03-10 21:17:04

--- handlers/wxWidgets/postMakeAvailable.cmake
+++ handlers/wxWidgets/postMakeAvailable.cmake
@@ -20,61 +20,112 @@ function(wxWidgets_postMakeAvailable sourceDir buildDir outDir buildType)
     _collect_targets_recursive("${buildDir}" _wx_targets)
     message(STATUS "wx targets: ${_wx_targets}")
 
-    foreach(t IN LISTS _wx_targets)
+    set(_secondTime OFF)
+    set(_triggered "BANG!")
+    set(local_includes "")
+
+    foreach(t IN LISTS _wx_targets _triggered _wx_targets)
+
+        if(t STREQUAL "BANG!")
+            set(_secondTime ON)
+        endif ()
+
         get_target_property(type "${t}" TYPE)
-            msg("${t}")
+        msg("${t}")
+
+        if (NOT _secondTime)
+
             get_target_property(_raw_includes "${t}" INTERFACE_INCLUDE_DIRECTORIES)
-            if (NOT _raw_includes)
-                if(type STREQUAL "INTERFACE_LIBRARY" OR type STREQUAL "SHARED_LIBRARY")
-                    set(_wx_incs "${sourceDir}/include/wx")
-                    file(GLOB_RECURSE _wx_setup_incs  LIST_DIRECTORIES true "${_wx_incs}")
-                    foreach(inc IN LISTS _wx_setup_incs) # _wx_setup_incs)
-                        if(IS_DIRECTORY "${inc}")
-                            if (inc MATCHES ".*wx$")
-                                get_filename_component(inc "${inc}" PATH)
-                                if((NOT WIN32 AND NOT inc MATCHES ".*msvc$") AND NOT inc MATCHES ".*_deps.*")
-                                    list(APPEND local_includes "$<BUILD_INTERFACE:${inc}>")
-                                endif ()
-                            endif ()
-                        endif ()
-                    endforeach ()
-                    list(REMOVE_DUPLICATES local_includes)
-                    msg(ALWAYS "\"${t}\": Setting INTERFACE_INCLUDE_DIRECTORIES to \"${local_includes}\"")
-                    list(APPEND _wxIncludePaths "${local_includes}")
-                    set(_wxIncludePaths "${_wxIncludePaths}" PARENT_SCOPE)
-                    set_property(TARGET "${t}" APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${local_includes}")
-                endif ()
-            else ()
-                set(BranNeuDae "")
-                foreach(thing IN LISTS _raw_includes)
-                    msg("${thing} from ${t}")
-                    if(thing MATCHES "/wx-[0-9]+\\.[0-9]+")
-                        msg("removing ${thing} from ${t}")
-                    else ()
-                        list(APPEND BranNeuDae "${thing}")
-                    endif ()
-                endforeach ()
-                set_property(TARGET "${t}" PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${BranNeuDae}")
+
+            if (_raw_includes)
+                msg("Grabbing \"${_raw_includes}\" from ${t}")
+                list(APPEND local_includes ${_raw_includes})
+                list(REMOVE_DUPLICATES local_includes)
             endif ()
-#        endif ()
 
-        if(NOT type STREQUAL "INTERFACE_LIBRARY" AND NOT type STREQUAL "UTILITY")
-            target_compile_options("${t}" PRIVATE -w)
-        endif()
-
-        # Force wx shared libs to be relocatable within the stage/install tree.
-        # This fixes cases like libwx_qtu depending on libwxwebp* in the same dir.
-        if(type STREQUAL "SHARED_LIBRARY" OR type STREQUAL "MODULE_LIBRARY")
-
-            if(UNIX AND NOT APPLE)
-                set_target_properties("${t}" PROPERTIES
-                        INSTALL_RPATH "\$ORIGIN"
-                        BUILD_RPATH   "\$ORIGIN"
-                        INSTALL_RPATH_USE_LINK_PATH FALSE
-                )
+            if(NOT type STREQUAL "INTERFACE_LIBRARY" AND NOT type STREQUAL "UTILITY")
+                target_compile_options("${t}" PRIVATE -w)
             endif()
-        endif()
-    endforeach()
+
+            # Force wx shared libs to be relocatable within the stage/install tree.
+            # This fixes cases like libwx_qtu depending on libwxwebp* in the same dir.
+            if(type STREQUAL "SHARED_LIBRARY" OR type STREQUAL "MODULE_LIBRARY")
+
+                if(UNIX AND NOT APPLE)
+                    set_target_properties("${t}" PROPERTIES
+                            INSTALL_RPATH "\$ORIGIN"
+                            BUILD_RPATH   "\$ORIGIN"
+                            INSTALL_RPATH_USE_LINK_PATH FALSE
+                    )
+                endif()
+            endif()
+
+        else ()
+
+            msg(ALWAYS "\"${t}\": Setting INTERFACE_INCLUDE_DIRECTORIES to \"${local_includes}\"")
+            list(APPEND _wxIncludePaths "${local_includes}")
+            set(_wxIncludePaths "${_wxIncludePaths}" PARENT_SCOPE)
+            set_property(TARGET "${t}" APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${local_includes}")
+
+        endif ()
+    endforeach ()
+##
+##
+##
+##                if(type STREQUAL "INTERFACE_LIBRARY" OR type STREQUAL "SHARED_LIBRARY")
+##                    set(_wx_incs "${sourceDir}/include/wx")
+##                    file(GLOB_RECURSE _wx_setup_incs  LIST_DIRECTORIES true "${_wx_incs}")
+##                    foreach(inc IN LISTS _wx_setup_incs) # _wx_setup_incs)
+##                        if(IS_DIRECTORY "${inc}")
+##                            if (inc MATCHES ".*wx$")
+##                                get_filename_component(inc "${inc}" PATH)
+##                                if((NOT WIN32 AND NOT inc MATCHES ".*msvc$") AND NOT inc MATCHES ".*_deps.*")
+##                                    list(APPEND local_includes "$<BUILD_INTERFACE:${inc}>")
+##                                endif ()
+##                            endif ()
+##                        endif ()
+##                    endforeach ()
+##                    list(REMOVE_DUPLICATES local_includes)
+##                    msg(ALWAYS "\"${t}\": Setting INTERFACE_INCLUDE_DIRECTORIES to \"${local_includes}\"")
+##                    list(APPEND _wxIncludePaths "${local_includes}")
+##                    set(_wxIncludePaths "${_wxIncludePaths}" PARENT_SCOPE)
+##                    set_property(TARGET "${t}" APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${local_includes}")
+##                endif ()
+##            else ()
+##                set(BranNeuDae "")
+##                foreach(thing IN LISTS _raw_includes)
+##                    msg("${thing} from ${t}")
+##                    if(thing MATCHES "/wx-[0-9]+\\.[0-9]+")
+##                        msg("removing ${thing} from ${t}")
+##                    else ()
+##                        list(APPEND BranNeuDae "${thing}")
+##                    endif ()
+##                endforeach ()
+##                set_property(TARGET "${t}" PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${BranNeuDae}")
+##            endif ()
+###        endif ()
+##
+##        if(_secondTime)
+##            continue()
+##        endif ()
+#
+#        if(NOT type STREQUAL "INTERFACE_LIBRARY" AND NOT type STREQUAL "UTILITY")
+#            target_compile_options("${t}" PRIVATE -w)
+#        endif()
+#
+#        # Force wx shared libs to be relocatable within the stage/install tree.
+#        # This fixes cases like libwx_qtu depending on libwxwebp* in the same dir.
+#        if(type STREQUAL "SHARED_LIBRARY" OR type STREQUAL "MODULE_LIBRARY")
+#
+#            if(UNIX AND NOT APPLE)
+#                set_target_properties("${t}" PROPERTIES
+#                        INSTALL_RPATH "\$ORIGIN"
+#                        BUILD_RPATH   "\$ORIGIN"
+#                        INSTALL_RPATH_USE_LINK_PATH FALSE
+#                )
+#            endif()
+#        endif()
+#    endforeach()
 
     wxWidgets_export_variables(wxWidgets)
 
