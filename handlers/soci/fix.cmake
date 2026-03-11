aadfdf78a29e866643fffd3eae5a29c7434a0b08
geoffrey <gethro2k@gmail.com>
2026-03-11 22:35:01 +1000

    Auto-sync: 2026-03-11 22:35:01

--- handlers/soci/fix.cmake
+++ handlers/soci/fix.cmake
@@ -2,25 +2,25 @@ include("${cmake_root}/tools.cmake")
 
 function(soci_fix target tag sourceDir)
 
-    if (NOT soci_PATCHED)
+#    if (NOT soci_PATCHED)
         unset(patches)
         list(APPEND patches
                 # Test whole folder
-                "${target}/3rdparty|${_soci_local_src}/3rdparty"
+                "${target}/3rdparty|${sourceDir}/3rdparty"
                 # Test single file
                 "${target}/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
 
-                "${target}/include|${_soci_local_src}/include"
+                "${target}/include|${sourceDir}/include"
 
-                "${target}/CMakeLists.txt|${_soci_local_src}"
-                "${target}/cmake/soci_define_backend_target.cmake|${_soci_local_src}/cmake"
+                "${target}/CMakeLists.txt|${sourceDir}"
+                "${target}/cmake/soci_define_backend_target.cmake|${sourceDir}/cmake"
 
-                "${target}/src|${_soci_local_src}/src"
+                "${target}/src|${sourceDir}/src"
         )
 
         replaceFiles(soci "${patches}")
-    endif ()
-    set(soci_PATCHED ON PARENT_SCOPE)
+#    endif ()
+#    set(soci_PATCHED ON PARENT_SCOPE)
 
     set(HANDLED ON PARENT_SCOPE)
 endfunction()
