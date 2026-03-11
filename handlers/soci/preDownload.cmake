464cf67da12ad9bf5bc552287a6f4e034595ac36
geoffrey <gethro2k@gmail.com>
2026-03-12 00:26:14 +1000

    Auto-sync: 2026-03-12 00:26:14

--- handlers/soci/preDownload.cmake
+++ handlers/soci/preDownload.cmake
@@ -91,19 +91,19 @@ function(soci_preDownload pkgname url tag srcDir)
 
     if (NOT soci_PATCHED)
         unset(patches)
-        set(_soci_local_src "${${target}_SOURCE_DIR}")
+        set(_soci_local_src "${soci_SOURCE_DIR}")
         list(APPEND patches
                 # Test whole folder
-                "${target}/3rdparty|${_soci_local_src}/3rdparty"
+                "soci/3rdparty|${_soci_local_src}/3rdparty"
                 # Test single file
-                "${target}/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
+                "soci/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
 
-                "${target}/include|${_soci_local_src}/include"
+                "soci/include|${_soci_local_src}/include"
 
-                "${target}/CMakeLists.txt|${_soci_local_src}"
-                #                "${target}/cmake/soci_define_backend_target.cmake|${_soci_local_src}/cmake"
+                "soci/CMakeLists.txt|${_soci_local_src}"
+                #                "soci/cmake/soci_define_backend_target.cmake|${_soci_local_src}/cmake"
 
-                "${target}/src|${_soci_local_src}/src"
+                "soci/src|${_soci_local_src}/src"
         )
 
         replaceFiles(soci "${patches}")
