7e2beb62ba18c7b0f9e6c3ad0a84b39a43c224d0
geoffrey <gethro2k@gmail.com>
2026-03-12 00:52:49 +1000

    Auto-sync: 2026-03-12 00:52:48

--- handlers/soci/preDownload.cmake
+++ handlers/soci/preDownload.cmake
@@ -98,8 +98,8 @@ function(soci_preDownload pkgname url tag srcDir)
                 "soci/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
 
                 "soci/include|${_soci_local_src}/include"
-#
-#                "soci/CMakeLists.txt|${_soci_local_src}"
+
+                "soci/CMakeLists.txt|${_soci_local_src}"
 #                "soci/cmake/soci_define_backend_target.cmake|${_soci_local_src}/cmake"
 #
 #                "soci/src|${_soci_local_src}/src"
@@ -115,8 +115,8 @@ function(soci_preDownload pkgname url tag srcDir)
 #                "soci/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
 #
 #                "soci/include|${_soci_local_src}"
-
-                "soci/CMakeLists.txt|${_soci_local_src}"
+#
+#                "soci/CMakeLists.txt|${_soci_local_src}"
                 "soci/cmake/soci_define_backend_target.cmake|${_soci_local_src}"
 
                 #            "soci/src/core/CMakeLists.txt|${sourceDir}"
