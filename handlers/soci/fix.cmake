1b7146834d4c5d7bf68d233c19bfeca7dc4394c9
geoffrey <gethro2k@gmail.com>
2026-03-11 22:32:37 +1000

    Auto-sync: 2026-03-11 22:32:37

--- handlers/soci/fix.cmake
+++ handlers/soci/fix.cmake
@@ -6,16 +6,16 @@ function(soci_fix target tag sourceDir)
         unset(patches)
         list(APPEND patches
                 # Test whole folder
-                "soci/3rdparty|${_soci_local_src}/3rdparty"
+                "${target}/3rdparty|${_soci_local_src}/3rdparty"
                 # Test single file
-                "soci/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
+                "${target}/3rdparty/fmt/include/fmt/base.h|${BUILD_DIR}/fmt-src/include/fmt/"
 
-                "soci/include|${_soci_local_src}/include"
+                "${target}/include|${_soci_local_src}/include"
 
-                "soci/CMakeLists.txt|${_soci_local_src}"
-                "soci/cmake/soci_define_backend_target.cmake|${_soci_local_src}/cmake"
+                "${target}/CMakeLists.txt|${_soci_local_src}"
+                "${target}/cmake/soci_define_backend_target.cmake|${_soci_local_src}/cmake"
 
-                "soci/src|${_soci_local_src}/src"
+                "${target}/src|${_soci_local_src}/src"
         )
 
         replaceFiles(soci "${patches}")
