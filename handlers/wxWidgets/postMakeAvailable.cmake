7cdcb504a14f12766c277b3aaf6cb09d4a1c8a04
geoffrey <gethro2k@gmail.com>
2026-03-23 18:44:59 +1000

    Auto-sync: 2026-03-23 18:44:59

--- handlers/wxWidgets/postMakeAvailable.cmake
+++ handlers/wxWidgets/postMakeAvailable.cmake
@@ -15,7 +15,6 @@ function(_collect_targets_recursive dir out)
 endfunction()
 
 function(wxWidgets_postMakeAvailable sourceDir buildDir outDir buildType)
-    msg("\nCMAKE_CURRENT_FUNCTION_LIST_DIR=${CMAKE_CURRENT_FUNCTION_LIST_DIR},\nsourceDir=${sourceDir},\nbuildDir=${buildDir},\noutDir=${outDir},buildType=${buildType}\n")
     include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/helpers.cmake)
 
     # Generate Wayland protocol headers/sources from XML on Linux
@@ -31,16 +30,13 @@ function(wxWidgets_postMakeAvailable sourceDir buildDir outDir buildType)
             set(_source "${_wx_protocols_out_dir}/${_proto_name}-client-protocol.c")
             if (NOT EXISTS "${_header}")
                 execute_process(COMMAND "${WAYLAND_SCANNER}" client-header "${_xml}" "${_header}")
-                msg("Generated Wayland protocol header: ${_header}")
             endif ()
             if (NOT EXISTS "${_source}")
                 execute_process(COMMAND "${WAYLAND_SCANNER}" private-code "${_xml}" "${_source}")
-                msg("Generated Wayland protocol source: ${_source}")
             endif ()
         endforeach ()
     endif ()
     _collect_targets_recursive("${buildDir}" _wx_targets)
-    message(STATUS "wx targets: ${_wx_targets}")
 
     set(_secondTime OFF)
     set(_triggered "BANG!")
@@ -54,14 +50,12 @@ function(wxWidgets_postMakeAvailable sourceDir buildDir outDir buildType)
         endif ()
 
         get_target_property(type "${t}" TYPE)
-        msg("${t}")
 
         if (NOT _secondTime)
 
             get_target_property(_raw_includes "${t}" INTERFACE_INCLUDE_DIRECTORIES)
 
             if (_raw_includes)
-                msg("Grabbing \"${_raw_includes}\" from ${t}")
                 list(APPEND local_includes ${_raw_includes})
                 list(REMOVE_DUPLICATES local_includes)
             endif ()
@@ -85,70 +79,12 @@ function(wxWidgets_postMakeAvailable sourceDir buildDir outDir buildType)
 
         else ()
 
-            msg(ALWAYS "\"${t}\": Setting INTERFACE_INCLUDE_DIRECTORIES to \"${local_includes}\"")
             list(APPEND _wxIncludePaths "${local_includes}")
             set(_wxIncludePaths "${_wxIncludePaths}" PARENT_SCOPE)
             set_property(TARGET "${t}" APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${local_includes}")
 
         endif ()
     endforeach ()
-##
-##
-##
-##                if(type STREQUAL "INTERFACE_LIBRARY" OR type STREQUAL "SHARED_LIBRARY")
-##                    set(_wx_incs "${sourceDir}/include/wx")
-##                    file(GLOB_RECURSE _wx_setup_incs  LIST_DIRECTORIES true "${_wx_incs}")
-##                    foreach(inc IN LISTS _wx_setup_incs) # _wx_setup_incs)
-##                        if(IS_DIRECTORY "${inc}")
-##                            if (inc MATCHES ".*wx$")
-##                                get_filename_component(inc "${inc}" PATH)
-##                                if((NOT WIN32 AND NOT inc MATCHES ".*msvc$") AND NOT inc MATCHES ".*_deps.*")
-##                                    list(APPEND local_includes "$<BUILD_INTERFACE:${inc}>")
-##                                endif ()
-##                            endif ()
-##                        endif ()
-##                    endforeach ()
-##                    list(REMOVE_DUPLICATES local_includes)
-##                    msg(ALWAYS "\"${t}\": Setting INTERFACE_INCLUDE_DIRECTORIES to \"${local_includes}\"")
-##                    list(APPEND _wxIncludePaths "${local_includes}")
-##                    set(_wxIncludePaths "${_wxIncludePaths}" PARENT_SCOPE)
-##                    set_property(TARGET "${t}" APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${local_includes}")
-##                endif ()
-##            else ()
-##                set(BranNeuDae "")
-##                foreach(thing IN LISTS _raw_includes)
-##                    msg("${thing} from ${t}")
-##                    if(thing MATCHES "/wx-[0-9]+\\.[0-9]+")
-##                        msg("removing ${thing} from ${t}")
-##                    else ()
-##                        list(APPEND BranNeuDae "${thing}")
-##                    endif ()
-##                endforeach ()
-##                set_property(TARGET "${t}" PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${BranNeuDae}")
-##            endif ()
-###        endif ()
-##
-##        if(_secondTime)
-##            continue()
-##        endif ()
-#
-#        if(NOT type STREQUAL "INTERFACE_LIBRARY" AND NOT type STREQUAL "UTILITY")
-#            target_compile_options("${t}" PRIVATE -w)
-#        endif()
-#
-#        # Force wx shared libs to be relocatable within the stage/install tree.
-#        # This fixes cases like libwx_qtu depending on libwxwebp* in the same dir.
-#        if(type STREQUAL "SHARED_LIBRARY" OR type STREQUAL "MODULE_LIBRARY")
-#
-#            if(UNIX AND NOT APPLE)
-#                set_target_properties("${t}" PROPERTIES
-#                        INSTALL_RPATH "\$ORIGIN"
-#                        BUILD_RPATH   "\$ORIGIN"
-#                        INSTALL_RPATH_USE_LINK_PATH FALSE
-#                )
-#            endif()
-#        endif()
-#    endforeach()
 
     wxWidgets_export_variables(wxWidgets)
 
