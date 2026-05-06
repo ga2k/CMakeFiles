047a9d2c1f8fd77e03f62b8694bbfea5da7ba6b9
geoffrey <gethro2k@gmail.com>
2026-05-06 16:42:55 +1000

    Unknown changes

--- FindGfx/init.cmake
+++ FindGfx/init.cmake
@@ -1,10 +1,6 @@
 
 function(addGfxFeatures dry_run)
     # @formatter:off
-    addPackageData(PLUGIN   FEATURE "APPEARANCE"  PKGNAME "Appearance"    METHOD "IGNORE" DRY_RUN ${dry_run})
-    addPackageData(PLUGIN   FEATURE "LOGGER"      PKGNAME "Logger"        METHOD "IGNORE" DRY_RUN ${dry_run})
-    addPackageData(PLUGIN   FEATURE "PRINT"       PKGNAME "Print"         METHOD "IGNORE" DRY_RUN ${dry_run})
-
     if(APP_NAME STREQUAL "Gfx")
         # Building Gfx itself: fetch wxWidgets from source and build it.
         addPackageData(OPTIONAL FEATURE "GUI" PKGNAME "wxWidgets" METHOD "FETCH_CONTENTS"
@@ -21,15 +17,6 @@ function(addGfxFeatures dry_run)
             ARGS REQUIRED CONFIG DRY_RUN ${dry_run})
     # @formatter:on
 
-#    if(Appearance IN_LIST APP_FEATURES)
-        registerPlugin("Appearance")
-#    endif ()
-#    if(Logger IN_LIST APP_FEATURES)
-        registerPlugin("Logger")
-#    endif ()
-#    if(Print IN_LIST APP_FEATURES)
-        registerPlugin("Print")
-#    endif ()
 
 endfunction()
 
