8b1929d8023e5b0f83919cda1b2e05e692d13f13
geoffrey <gethro2k@gmail.com>
2026-06-04 20:20:38 +1000

    Bug fixing move of cpphttplib from MyHealthGuru to Core

--- handlers/cpp-httplib/preMakeAvailable.cmake
+++ handlers/cpp-httplib/preMakeAvailable.cmake
@@ -3,7 +3,7 @@ include("${cmake_root}/tools.cmake")
 
 function(cpp-httplib_preMakeAvailable pkgname)
 
-    set(HTTPLIB_REQUIRE_OPENSSL ON)
+    set(HTTPLIB_REQUIRE_OPENSSL ON PARENT_SCOPE)
     set(HANDLED OFF)
     set(HANDLED ${HANDLED} PARENT_SCOPE)
 
