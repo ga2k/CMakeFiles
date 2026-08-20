52a40955f5c023cc2e625b769e0bf2416fe99da7
geoffrey <gethro2k@gmail.com>
2026-08-20 14:53:14 +1000

    things

--- handlers/wxWidgets/fix.cmake
+++ handlers/wxWidgets/fix.cmake
@@ -4,14 +4,16 @@ function(wxWidgets_fix target tag sourceDir)
 
    cmake_policy(SET CMP0111 OLD)
 
-   message(FATAL_ERROR "sourceDir=${sourceDir}")
-   
-#   unset(patches)
-#   list(APPEND patches
-#           ${target}|${sourceDir}
-#           "${target}/src|${sourceDir}/src"
-#   )
-#   replaceFiles(${target} "${patches}")
+   file(READ "${sourceDir}/include/wx/defs.h" contents)
+   string(SUBSTRING "${contents}" 1 6 PRAGMA)
+   if("${PRAGMA}" STREQUAL "pragma")
+       message("'${PRAGMA}' is 'pragma'")
+   else()
+       message(WARNING "'${PRAGMA}' is not 'pragma'")
+       set(contents "#pragma once\n${contents}")
+       file(WRITE "${sourceDir}/include/wx/defs.h" "${contents}")
+   endif()
+
    set(HANDLED ON PARENT_SCOPE)
 
 endfunction()
