5e0848208c284393c3423767abe9d1f42ac6ff1e
geoffrey <gethro2k@gmail.com>
2026-09-12 12:52:01 +1000

    stuff

--- handlers/wxWidgets/fix.cmake
+++ handlers/wxWidgets/fix.cmake
@@ -1,19 +1,25 @@
 include("${cmake_root}/tools.cmake")
 
-function(wxWidgets_fix target tag sourceDir)
-
-   cmake_policy(SET CMP0111 OLD)
-
-   file(READ "${sourceDir}/include/wx/defs.h" contents)
+function(add_pragma_once path)
+   file(READ "${path}" contents)
    string(SUBSTRING "${contents}" 1 6 PRAGMA)
    if("${PRAGMA}" STREQUAL "pragma")
        message("'${PRAGMA}' is 'pragma'")
    else()
        message(WARNING "'${PRAGMA}' is not 'pragma'")
        set(contents "#pragma once\n${contents}")
-       file(WRITE "${sourceDir}/include/wx/defs.h" "${contents}")
+       file(WRITE "${path}" "${contents}")
    endif()
+endfunction()
+
+function(wxWidgets_fix target tag sourceDir)
+
+   cmake_policy(SET CMP0111 OLD)
+
+   add_pragma_once("${sourceDir}/include/wx/defs.h")
+   add_pragma_once("${sourceDir}/include/wx/wxcrtvararg.h")
 
    set(HANDLED ON PARENT_SCOPE)
 
 endfunction()
+
